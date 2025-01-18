d3fae1ddc3e7ba844ccb8639fa6461c03098463d
basecomust <mynewgma1lacccausewhynot@gmail.com>
2025-01-18 10:25:59 +0400

    如果你能讀到這篇文章，你就是同性戀

---
+++ script.js
@@ -0,0 +1,20 @@
+function anglify() {
+	document.getElementById("introduction").innerHTML = "Greetings, comrade!";
+	document.getElementById("theproblem").innerHTML = "This website is reserved for the intergalactic communist empire <b>BASE COMUST</b>.";
+	document.getElementById("thesis").innerHTML = "You are stationed at the station <b>POLUS</b>.";
+	document.getElementById("conclusion").innerHTML = "Enjoy your stay, comrade!";
+}
+
+function ñify() {
+	document.getElementById("introduction").innerHTML = "¡Buenas tardes, camarada!";
+	document.getElementById("theproblem").innerHTML = "Este sitio web está reservado para el imperio comunista intergaláctico <b>BASE COMUST</b>.";
+	document.getElementById("thesis").innerHTML = "Estás estacionado en la estación <b>POLUS</b>.";
+	document.getElementById("conclusion").innerHTML = "¡Disfruta tu estancia, camarada!";
+}
+
+function jugoslavify() {
+	document.getElementById("introduction").innerHTML = "Поздрав, друже!";
+	document.getElementById("theproblem").innerHTML = "Ова веб страница је резервисана за међугалактичко комунистичко царство <b>БАЗНИ КОМУСТ</b>.";
+	document.getElementById("thesis").innerHTML = "Стационирани сте на станици <b>ПОЛУС</b>.";
+	document.getElementById("conclusion").innerHTML = "Уживајте у боравку, друже!";
+}
